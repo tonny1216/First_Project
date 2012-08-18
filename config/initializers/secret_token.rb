ce9bdb444f1f0a894bb3e39bdbf34c6751c96c9e
U-WIN-D84DBKCNMG2\Eric <Eric@WIN-D84DBKCNMG2.(none)>
2012-08-18 14:27:39 -0700

    initial commot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0fe86af4a8827bdc46f467be61730fde5e7d5cf7d99c7e32f75184b9de6029c67ad0479b19d3b36e081dce539d0cfe4fbff04d725839bfa803ae46ca02e9393f'
